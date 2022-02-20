e187270490a31c69179b74b070d318bf36d71b3e
Sauvik Chatterjee <73000146+sauvikchatterjee07@users.noreply.github.com>
2022-02-20 18:26:21 +0530

    Create 278. First Bad Version.cpp

---
+++ 278. First Bad Version.cpp
@@ -0,0 +1,12 @@
+    int firstBadVersion(int n) {
+        int left = 0;
+        int right = n;
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(isBadVersion(mid))
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
